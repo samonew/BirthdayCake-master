6a06229db6abe7b385133e68e3c86be47770fca0
samonewatkins <watkinss23@up.edu>
2020-02-03 21:50:48 -0800

    Lab 3

---
+++ app/src/main/java/cs301/birthdaycake/cakeController.java
@@ -0,0 +1,53 @@
+package cs301.birthdaycake;
+
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+
+public class cakeController implements View.OnClickListener, CompoundButton.OnCheckedChangeListener,
+            SeekBar.OnSeekBarChangeListener{
+
+
+    private CakeView cakeView;
+    private CakeModel cakecake;
+
+    public cakeController(CakeView cake){
+
+        cakeView = cake;
+        cakecake = cakeView.getCake();
+
+
+    }
+
+
+    public void onClick( View view ){
+
+        cakecake.areLit = false;
+        cakeView.invalidate();
+
+
+    }
+
+    public void onCheckedChanged(CompoundButton button, boolean b ){
+
+        cakecake.hasCandles = false;
+        cakeView.invalidate();
+
+    }
+
+    public void onProgressChanged(SeekBar seek, int i, boolean b){
+
+        cakecake.numCandles = i;
+        cakeView.invalidate();
+
+    }
+
+    public void onStartTrackingTouch(SeekBar seek){
+
+    }
+
+    public void onStopTrackingTouch(SeekBar seek){
+
+    }
+
+}
